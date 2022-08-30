85e1e2e53cc85b635de75984d2a0f5232c3c0db2
ManUtdMoon <yudongjie.moon@foxmail.com>
2022-08-30 11:05:17 +0800

    code clean and sync

--- run.sh
+++ run.sh
@@ -3,4 +3,11 @@
 PARENT_DIR=$(cd $(dirname $0);cd ..; pwd)
 export PYTHONPATH=$PYTHONPATH:$PARENT_DIR
 
-python main.py -c config/quadrotor-csc.json  # quadrotor cartpole-move
+# quadrotor cartpole-move
+python main.py -c config/quadrotor-mbpol.json
+python main.py -c config/quadrotor-csc.json
+
+python main.py -c config/quadrotor-mbpol.json \
+    -s seed 7465
+python main.py -c config/quadrotor-csc.json \
+    -s seed 7465
