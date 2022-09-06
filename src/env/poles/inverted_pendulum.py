7d2b1ae22549da32fe7f074b0d6ef3c3a5441e98
ManUtdMoon <yudongjie.moon@foxmail.com>
2022-09-06 10:14:17 +0800

    test rmbpo on cartpole but not so good

--- src/env/poles/inverted_pendulum.py
+++ src/env/poles/inverted_pendulum.py
@@ -8,7 +8,7 @@ from .constraints import BoundedConstraint, ConstrainedVariableType
 
 class SafeInvertedPendulumEnv(InvertedPendulumEnv):
 
-    def __init__(self, threshold=0.2, task='upright'):
+    def __init__(self, threshold=0.2, task='upright', id=None):
         self.task = task
         self.th_threshold = threshold
         self.th_margin = 0.1
@@ -37,6 +37,7 @@ class SafeInvertedPendulumEnv(InvertedPendulumEnv):
         self.con_dim = 4  # lb & ub
 
         self._max_episode_steps = 1000
+        self.done_on_violation = (id is None)
         super().__init__()
         EzPickle.__init__(self, threshold=threshold, task=task)  # deepcopy calls `get_state`
 
@@ -71,12 +72,12 @@ class SafeInvertedPendulumEnv(InvertedPendulumEnv):
             constraint_value=constraint_value
         )
 
-        done = np.any(self._soft_constraint_values(next_state) > 0.).item()
+        done = violation if self.done_on_violation else self.soft_constraints.is_violated(next_state)
 
         return next_state, reward, done, info
 
     def check_done(self, states: np.array):
-        larger_violations = self.soft_constraints.is_violated(states)
+        larger_violations = self.constraints.is_violated(states)
         assert len(larger_violations.shape) == 1
         return larger_violations
     
