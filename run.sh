979c75a80bd99802196d5be30f7c14b926991ae0
ManUtdMoon <yudongjie.moon@foxmail.com>
2022-09-10 10:54:39 +0800

    run safetygym-car with 1 & 2 std

--- run.sh
+++ run.sh
@@ -4,14 +4,8 @@ PARENT_DIR=$(cd $(dirname $0);cd ..; pwd)
 export PYTHONPATH=$PYTHONPATH:$PARENT_DIR
 
 # env: safetygym-car safetygym-point
-python main.py -c config/safetygym-car.json \
-    -s seed 7346588
-
-python main.py -c config/safetygym-car.json \
-    -s seed 1
-
-python main.py -c config/safetygym-car.json \
-    -s seed 49283
-
-python main.py -c config/safetygym-car.json \
-    -s seed 789
+for i in 49283 789
+do
+    python main.py -c config/safetygym-car.json \
+        -s seed $i
+done
