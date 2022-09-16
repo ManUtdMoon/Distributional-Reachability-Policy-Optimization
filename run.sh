d6de363250c6df5ec142a01ca60c8c7e6ad1554b
ManUtdMoon <yudongjie.moon@foxmail.com>
2022-09-16 10:35:14 +0800

    sync settings of csc, sacl, mbpol on cartpole

--- run.sh
+++ run.sh
@@ -4,49 +4,8 @@ PARENT_DIR=$(cd $(dirname $0);cd ..; pwd)
 export PYTHONPATH=$PYTHONPATH:$PARENT_DIR
 
 # quadrotor cartpole-move
-# python main.py -c config/quadrotor-mbpol.json \
-#     -s seed 64578
-# python main.py -c config/quadrotor-sacl.json \
-#     -s seed 64578
-
-# python main.py -c config/quadrotor-mbpol.json \
-#     -s seed 219803
-# python main.py -c config/quadrotor-sacl.json \
-#     -s seed 219803
-
-# python main.py -c config/quadrotor-mbpol.json \
-#     -s seed 456
-# python main.py -c config/quadrotor-sacl.json \
-#     -s seed 456
-
-# python main.py -c config/quadrotor-mbpol.json \
-#     -s seed 1
-# python main.py -c config/quadrotor-sacl.json \
-#     -s seed 1
-
-# python main.py -c config/quadrotor-mbpol.json \
-#     -s seed 43567
-# python main.py -c config/quadrotor-sacl.json \
-#     -s seed 43567
-
-# python main.py -c config/cartpole-move-mbpol.json \
-#     -s seed 49283
-# python main.py -c config/cartpole-move-mbpol.json \
-#     -s seed 43567
-# python main.py -c config/cartpole-move-mbpol.json \
-#     -s seed 7346588
-# python main.py -c config/cartpole-move-mbpol.json \
-#     -s seed 49283
-# python main.py -c config/cartpole-move-mbpol.json \
-#     -s seed 64578
-
-python main.py -c config/cartpole-move-sacl.json \
-    -s seed 49283
-python main.py -c config/cartpole-move-sacl.json \
-    -s seed 43567
-python main.py -c config/cartpole-move-sacl.json \
-    -s seed 7346588
-python main.py -c config/cartpole-move-sacl.json \
-    -s seed 49283
-python main.py -c config/cartpole-move-sacl.json \
-    -s seed 64578
+for i in 1 49283 64578 219803 4354
+do
+    CUDA_VISIBLE_DEVICES=1 python main.py -c config/cartpole-move-csc.json \
+        -s seed $i
+done
