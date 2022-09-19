c6cfcf17fc6fad9cdb8e3e4caafd4a3c4dd23bc8
ManUtdMoon <yudongjie.moon@foxmail.com>
2022-09-19 10:50:27 +0800

    add line search when eval

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ PARENT_DIR=$(cd $(dirname $0);cd ..; pwd)
 export PYTHONPATH=$PYTHONPATH:$PARENT_DIR
 
 # env: safetygym-car safetygym-point
-for i in 49283 789
+for i in 1 43567 49283 789 7346588
 do
     python main.py -c config/safetygym-car.json \
         -s seed $i
