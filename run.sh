5b9604b78fa7f8f8476a756beb4a3cc998003b45
ManUtdMoon <yudongjie.moon@foxmail.com>
2022-09-20 22:17:48 +0800

    add code and seeds for ablation_modules on quadrotor

--- run.sh
+++ run.sh
@@ -4,12 +4,38 @@ PARENT_DIR=$(cd $(dirname $0);cd ..; pwd)
 export PYTHONPATH=$PYTHONPATH:$PARENT_DIR
 
 # quadrotor cartpole-move
-
-for i in 5435
+# Vanilla
+for i in 43567 748365 219803 4354 64578
 do
-    python main.py -c config/cartpole-move.json \
+    python main.py -c config/quadrotor.json \
+        -s seed $i \
+        -s alg_cfg.eval_shield_type no
+done
+
+# Uncertainty only
+for i in 748365 6790 90 43567 4354
+do
+    python main.py -c config/quadrotor.json \
         -s seed $i \
         -s alg_cfg.sac_cfg.qc_under_uncertainty True \
         -s alg_cfg.sac_cfg.distributional_qc True \
+        -s alg_cfg.eval_shield_type no
+done
+
+# Shield only
+for i in 64578 219803 748365 57634 467
+do
+    python main.py -c config/quadrotor.json \
+        -s seed $i \
         -s alg_cfg.safe_shield True
+done
+
+# DRPO
+for i in 64578 219803 4354 43567 49283
+do
+    python main.py -c config/quadrotor.json \
+        -s seed $i \
+        -s alg_cfg.safe_shield True \
+        -s alg_cfg.sac_cfg.qc_under_uncertainty True \
+        -s alg_cfg.sac_cfg.distributional_qc True
 done
